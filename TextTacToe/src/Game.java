a03f86a4b45d0ec5d5221abd0776d8b988d9cd2d
Jordan Greenfield <green12j@cmich.edu>
2016-09-17 20:27:06 -0400

    Created source folder and documentation.

---
+++ TextTacToe/src/Game.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ * @author JGreeny
+ * 
+ * This program simulates the classic Tic-Tac-Toe game in a text form.
+ * 
+ * This game currently only supports two players, no player vs AI!
+ * 
+ * Once the game is started, the two players will each enter their name into
+ * the console. The players will take turns placing X's and O's into the 3x3 grid
+ * by choosing a number (1 through 9) that corresponds to the space they would
+ * like to play in. Once 1) the board is full or 2) a player places three of 
+ * their pieces in a row, the current match will end and the winner (if there
+ * is one) will be determined.
+ * 
+ * Each "game" can last as long as the players are willing to play, and consists
+ * of multiple matches. The game will keep score of the number of individual
+ * matches each player wins until they choose to end the game.
+ *
+ * After each turn, an updated board will display in the console so the players
+ * can see where the pieces are at and which spots are open.
+ * 
+ * After each match the players will be presented with a prompt with 3 options:
+ * 		1) Continue the current game (starting a new match).
+ * 		2) Start a new game (erase the players' scores and start over).
+ * 		3) Exit the game (terminate the program).
+ */
+
+public class Game {
+
+}
